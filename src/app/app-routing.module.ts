ee6e9960bce6cb88150b901d5689da9fe6f51cde
robertIsaac <robert.isaac@live.com>
2022-01-03 17:40:08 +0200

    initialize the project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'feature-one',
+    loadChildren: () => import('./feature-one/feature-one.module').then(m => m.FeatureOneModule),
+  },
+  {
+    path: 'feature-two',
+    loadChildren: () => import('./feature-two/feature-two.module').then(m => m.FeatureTwoModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/feature-one/feature-one.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-one',
+  templateUrl: './feature-one.component.html',
+  styleUrls: ['./feature-one.component.scss'],
+})
+export class FeatureOneComponent {
+
+  constructor() {
+  }
+
+}
